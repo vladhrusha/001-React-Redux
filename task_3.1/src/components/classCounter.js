ccb98eeefd1f955b844231242a4b734e79a27e03
Vlad Hrusha <hrushavladyslavwork@gmail.com>
2022-07-14 18:12:25 +0300

    counters functionality added

--- task_3.1/src/components/classCounter.js
+++ task_3.1/src/components/classCounter.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import { React, Component, useState } from 'react';
 
-class ClassCounter extends React.Component {
+class ClassCounter extends Component {
+  state = {
+    result: 0,
+  };
+
+  increase = () => {
+    // return (this.state.result += 1);
+    this.setState({ result: this.state.result + 1 });
+  };
+  decrease = () => {
+    this.setState({ result: this.state.result - 1 });
+  };
+  reset = () => {
+    this.setState({ result: 0 });
+  };
+  // getResult = () => {
+  //   return this.state.result;
+  // };
   render() {
     return (
       <div className='counter'>
         <h2>Class</h2>
-        <h3 className='result'>0</h3>
-        <h5 className='increase'>+</h5>
-        <h5 className='decrease'>-</h5>
-        <h4 className='reset'>Reset</h4>
+        <h3 className='result'>{this.state.result}</h3>
+        <h5 className='controlButton increaseButton' onClick={this.increase}>
+          +
+        </h5>
+        <h5 className='controlButton decreaseButton' onClick={this.decrease}>
+          -
+        </h5>
+        <h4 className='controlButton resetButton' onClick={this.reset}>
+          Reset
+        </h4>
       </div>
     );
   }
--- task_3.1/src/components/funcCounter.js
+++ task_3.1/src/components/funcCounter.js
@@ -1,11 +1,31 @@
+import { React, useState } from 'react';
+
 function FuncCounter() {
+  const [result, setResult] = useState(0);
+
+  const increase = () => {
+    setResult(result + 1);
+  };
+  const decrease = () => {
+    setResult(result - 1);
+  };
+  const reset = () => {
+    setResult(0);
+  };
+
   return (
     <div className='counter'>
       <h2>Func</h2>
-      <h3 className='result'>0</h3>
-      <h5 className='increase'>+</h5>
-      <h5 className='decrease'>-</h5>
-      <h4 className='reset'>Reset</h4>
+      <h3 className='result'>{result}</h3>
+      <h5 className='controlButton increaseButton' onClick={increase}>
+        +
+      </h5>
+      <h5 className='controlButton decreaseButton' onClick={decrease}>
+        -
+      </h5>
+      <h4 className='controlButton resetButton' onClick={reset}>
+        Reset
+      </h4>
     </div>
   );
 }
