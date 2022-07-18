a5867bc4634408b2d436b4c0496db344ac97e180
Vlad Hrusha <hrushavladyslavwork@gmail.com>
2022-07-18 21:07:16 +0300

    removed extra js files

--- task_3.3/src/components/Home.js
+++
@@ -1,7 +0,0 @@
-import '../styles/Main.scss';
-var Home = function () {
-    return (<div className='main'>
-    </div>);
-};
-export default Home;
-{ /* <img src={logo} className='App-logo' alt='logo' /> */ }
