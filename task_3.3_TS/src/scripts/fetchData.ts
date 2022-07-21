ceaa000083395429caf3e396086e0547e0e52e71
Vlad Hrusha <hrushavladyslavwork@gmail.com>
2022-07-21 18:09:47 +0300

    final commit

--- task_3.3_TS/src/scripts/fetchData.ts
+++ task_3.3_TS/src/scripts/fetchData.ts
@@ -1,8 +1,7 @@
 
-const API_URL : string = 'https://jsonplaceholder.typicode.com/'
 const {REACT_APP_API_URL} = process.env;
 const fetchData = async (resourceName : string) => {
-    let response = await fetch('https:/' + REACT_APP_API_URL + resourceName)
+    let response = await fetch(REACT_APP_API_URL + resourceName)
     let data = await response.json()
     return data
 }
