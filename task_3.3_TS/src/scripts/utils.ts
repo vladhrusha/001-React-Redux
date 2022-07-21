44bf2b7a8669fcd46e07e751d2ea6efa27a6d273
Vlad Hrusha <hrushavladyslavwork@gmail.com>
2022-07-22 00:53:51 +0300

    Update utils.ts

--- task_3.3_TS/src/scripts/utils.ts
+++ task_3.3_TS/src/scripts/utils.ts
@@ -1,6 +1,5 @@
 function isEmptyOrWhitespaceOnly(str : string){
     str = str.replace(/(<\/?[^>]+(>|$)|&nbsp;|\s)/g, "")
-    console.log('l' + str.toString() + 'k')
-    return str == '' || str.trim().length === 0
+    return str === '' || str.trim().length === 0
 }
 export {isEmptyOrWhitespaceOnly}
